0e5e75e3283fc0552342abd931922be63c7fb78f
Bob Kubinec <bobkubinec@gmail.com>
2021-03-03 16:25:56 +0400

    run this thing on the cluster!

--- run_cluster.R
+++ run_cluster.R
@@ -7,43 +7,44 @@ require(tidyr)
 
 this_mod <- Sys.getenv("MODTYPE")
 
+this_run <- Sys.getenv("THISRUN")
+
+rollcalls <- readRDS('data/rollcalls.rds') %>% 
+  select(cast_code,rollnumber,congress,
+         bioname,party_code,date_month,unemp_rate) %>% 
+  mutate(item=paste0(congress,"_",rollnumber),
+         cast_code=recode_factor(cast_code,Abstention=NA_character_),
+         cast_code=as.numeric(cast_code)-1,
+         bioname=factor(bioname),
+         bioname=relevel(bioname,"DeFAZIO, Peter Anthony")) %>% 
+  # filter(bioname %in% c("BARTON, Joe Linus",
+  #                       "DeFAZIO, Peter Anthony",
+  #                       "LEVIN, Sander Martin",
+  #                       "ROGERS, Harold Dallas (Hal)")) %>%
+  distinct
+
+# drop legislators who vote on fewer than 25 unanimous bills
+
+check_bills <- group_by(rollcalls,item,cast_code) %>% count
+
+legis_count <- group_by(rollcalls, item) %>% 
+  mutate(unan=all(cast_code[!is.na(cast_code)]==1) || all(cast_code[!is.na(cast_code)]==0)) %>% 
+  group_by(bioname) %>% 
+  summarize(n_votes_nonunam=length(unique(item[!unan])))
+
+# check number of days in legislature
+
+num_days <- distinct(rollcalls,bioname,date_month) %>% 
+  count(bioname)
+
+rollcalls <- anti_join(rollcalls, filter(legis_count, n_votes_nonunam<25),by="bioname") %>% 
+  anti_join(filter(num_days,n<10),by="bioname")
 
 if(this_mod=="first_ar") {
   
-  rollcalls <- readRDS('data/rollcalls.rds')
-  
-  unemp1 <- rollcalls %>% 
-    select(cast_code,rollnumber,congress,
-           bioname,party_code,date_month,unemp_rate) %>% 
-    mutate(item=paste0(congress,"_",rollnumber),
-           cast_code=recode_factor(cast_code,Abstention=NA_character_),
-           cast_code=as.numeric(cast_code)-1,
-           bioname=factor(bioname),
-           bioname=relevel(bioname,"DeFAZIO, Peter Anthony")) %>% 
-    filter(bioname %in% c("BARTON, Joe Linus",
-                          "DeFAZIO, Peter Anthony",
-                          "LEVIN, Sander Martin",
-                          "ROGERS, Harold Dallas (Hal)")) %>%
-    distinct
-  
-  # drop legislators who vote on fewer than 25 unanimous bills
-  
-  check_bills <- group_by(unemp1,item,cast_code) %>% count
-  
-  legis_count <- group_by(unemp1, item) %>% 
-    mutate(unan=all(cast_code[!is.na(cast_code)]==1) || all(cast_code[!is.na(cast_code)]==0)) %>% 
-    group_by(bioname) %>% 
-    summarize(n_votes_nonunam=length(unique(item[!unan])))
-  
-  # check number of days in legislature
-  
-  num_days <- distinct(unemp1,bioname,date_month) %>% 
-    count(bioname)
-  
   # you had to have voted on at least 10 separate days
   
-  unemp1 <- anti_join(unemp1, filter(legis_count, n_votes_nonunam<25),by="bioname") %>% 
-    anti_join(filter(num_days,n<10),by="bioname") %>% 
+  unemp1 <- rollcalls %>% 
     id_make(outcome_disc="cast_code",
             item_id="item",
             person_id="bioname",
@@ -76,21 +77,16 @@ if(this_mod=="first_ar") {
                             #include=F,
                             id_refresh=100)
   
-  saveRDS(unemp1_fit,'/scratch/rmk7/idalstan_compare/unemp1_fit.rds')
+  saveRDS(unemp1_fit,paste0('/scratch/rmk7/idalstan_compare/unemp1_',"run",this_run,'fit.rds'))
   
   
 } else if(this_mod=='gp_groups') {
   
-  rollcalls <- readRDS('data/rollcalls.rds')
+
   unemp2 <- rollcalls %>% 
-    select(cast_code,rollnumber,
-           bioname,party_code,date,unemp_rate) %>% 
-    mutate(cast_code=recode_factor(cast_code,Abstention=NA_character_),
-           cast_code=as.numeric(cast_code)-1) %>% 
-    distinct %>% 
     filter(date>lubridate::ymd("2008-06-01")) %>% 
     id_make(outcome_disc="cast_code",
-            item_id="rollnumber",
+            item_id="item",
             person_id="bioname",
             group_id="party_code",
             time_id = "date",
@@ -100,7 +96,7 @@ if(this_mod=="first_ar") {
   unemp2_fit <- id_estimate(unemp2,model_type=2,vary_ideal_pts = 'GP',
                             niters=300,
                             warmup=300,gpu=T,
-                            ncores=2,nchain=1,save_files="/scratch/rmk7/idalstan_compare/",
+                            ncores=12,nchains=4,save_files="/scratch/rmk7/idalstan_compare/",
                             fixtype="prefix",
                             restrict_ind_high = "R",
                             restrict_ind_low="D",
@@ -121,17 +117,13 @@ if(this_mod=="first_ar") {
   
 } else if(this_mod=="chinafit") {
   
-  rollcalls <- readRDS('data/rollcalls.rds') %>% 
-    select(cast_code,rollnumber,year,
-           bioname,party_code,date,unemp_rate,
-           district_code,state_abbrev) %>% 
-    mutate(cast_code=recode_factor(cast_code,Abstention=NA_character_),
-           cast_code=as.numeric(cast_code)-1) %>% 
+  rollcalls <- rollcalls %>% 
     distinct %>% 
     mutate(decade=case_when(year<2000~1L,
                             year<2010~2L,
                             TRUE~3L)) %>% 
     filter(decade<3)
+  
   load('data/andy_hall_jop/fh_final_analysis.rdata')
   
   x <- filter(x,!is.na(x)) %>% 
@@ -144,17 +136,17 @@ if(this_mod=="first_ar") {
   rollcalls <- filter(rollcalls,!is.na(x))
   
   china_data <- id_make(rollcalls,outcome_disc="cast_code",
-                        item_id="rollnumber",
+                        item_id="item",
                         person_id="bioname",
                         group_id="party_code",
                         time_id = "date",
                         remove_cov_int = T,
                         person_cov = ~unemp_rate*party_code*x)
-  rm(rollcalls)
+
   china_fit <- id_estimate(china_data,vary_ideal_pts = 'AR1',
                            niters=300,
                            warmup=300,gpu=F,
-                           ncores=parallel::detectCores(),nchains=1,
+                           ncores=8,nchains=4,
                            fixtype="prefix",save_files="/scratch/rmk7/idalstan_compare/",
                            restrict_ind_high = "R",
                            restrict_ind_low="D",
@@ -162,7 +154,7 @@ if(this_mod=="first_ar") {
                            cmdstan_path_user="/home/rmk7/cmdstan",
                            fix_low=0,
                            use_groups = T,
-                           map_over_id="items",
+                           map_over_id="persons",
                            within_chain="threads",
                            # output_samples=100,
                            # pars=c("steps_votes_grm",
