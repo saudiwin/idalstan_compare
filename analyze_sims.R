70d27b4f7f76aa200fe1ca26893a74940b1132d1
Bob Kubinec <bobkubinec@gmail.com>
2025-05-01 14:59:47 -0400

    go down to 1 restriction, increase proportion of missing data

--- analyze_sims.R
+++ analyze_sims.R
@@ -83,10 +83,12 @@ rse <- function(col1,col2) {
 # add in sign_rotation parameter
 
 sim_draws <- ungroup(sim_draws) %>% 
-  mutate(sign_rotation=ifelse(rse(ideal_point,true_ideal_point)>(rse(-1*ideal_point,
-                                                                      true_ideal_point)),
-                              1,0),
-         coef_rmse=rse(as.numeric(true_est_coef),as.numeric(est_coef)),
+  group_by(model, numitems, task_time, sim) %>% 
+  mutate(sign_rotation=ifelse(mean(rse(ideal_point,true_ideal_point),na.rm=T)>mean((rse(-1*ideal_point,
+                                                                      true_ideal_point)),na.rm=T),
+                              1,0)) %>% 
+  ungroup %>% 
+         mutate(coef_rmse=rse(as.numeric(true_est_coef),as.numeric(est_coef)),
          rmse_corrected=ifelse(sign_rotation,
                                rse(-1*ideal_point,
                                    true_ideal_point),
@@ -134,37 +136,18 @@ missing_rmse_norotate <- filter(sim_draws, sign_rotation==0) %>%
   summarize(mean_rmse=mean(rmse, na.rm=T),
             coef_rmse=mean(coef_rmse, na.rm=T))
 
-sim_draws %>% 
-  distinct(model,true_est_coef, est_coef, timeproc,missingness) %>% 
- # filter(timeproc=="GP") %>% 
-  ggplot(aes(y=true_est_coef,
-             x=est_coef)) +
-  geom_point() +
-  facet_wrap(~model) + 
-  geom_abline(slope=1, intercept=0, linetype=2, colour="red", size=2)
-
-# plot coverage rates
-
-calc_sum %>% 
-  ggplot(aes(x=cov_ideal)) +
-  geom_histogram() +
-  facet_wrap(~model) +
-  geom_vline(xintercept = .95, linetype=2)
-
-calc_sum %>% 
-  #filter(timeproc!="GP") %>% 
-  ggplot(aes(x=mean_rmse)) +
-  geom_histogram() +
-  facet_wrap(~model)
+# sim_draws %>% 
+#   distinct(model,true_est_coef, est_coef, timeproc,missingness) %>% 
+#  # filter(timeproc=="GP") %>% 
+#   ggplot(aes(y=true_est_coef,
+#              x=est_coef)) +
+#   geom_point() +
+#   facet_wrap(~model) + 
+#   geom_abline(slope=1, intercept=0, linetype=2, colour="red", size=2)
 
 calc_sum %>% 
   group_by(model) %>% 
   summarize(mean_cov=mean(cov_ideal,na.rm=T))
-
-calc_sum %>% 
-  group_by(model) %>% 
-  summarize(mean_rmse=mean(mean_rmse,na.rm=T)) %>% 
-  arrange(mean_rmse)
 
 calc_sum %>% 
   group_by(model,missingness) %>% 
@@ -175,11 +158,6 @@ calc_sum %>%
   group_by(model,missingness) %>% 
   summarize(mean_s=mean(s_error,na.rm=T)) %>% 
   arrange(mean_s)
-
-calc_sum %>% 
-  group_by(model,missingness) %>% 
-  summarize(mean_rmse=mean(mean_rmse,na.rm=T)) %>% 
-  arrange(mean_rmse)
   
   out_rmse <- sim_draws %>% 
   mutate(missingness=factor(missingness,labels=c("Ignorable","Non-ignorable")),
@@ -311,143 +289,59 @@ saveRDS(p4,"data/plot_coverage_corrected.rds")
 
 ggsave("plots/coverage_corrected.png",plot=p4)
 
-sim_draws %>% 
-  mutate(missingness=factor(missingness)) %>% 
-  ggplot(aes(y=coef_rmse_corrected,
-             x=reorder(model,coef_rmse))) +
-  stat_summary(fun.data="mean_cl_normal",
-               aes(linetype=missingness,
-                   colour=missingness),
-               position=position_dodge(.5)) +
-  facet_wrap(~timeproc,scales="free_y")
+out_sign_rotate <- sim_draws %>% 
+  mutate(missingness=factor(missingness,labels=c("Ignorable","Non-ignorable")),
+         timeproc=factor(timeproc, levels=c("AR","GP","randomwalk","splines"),
+                         labels=c("AR(1)","Gaussian Process","Random Walk","Spline"))) %>% 
+  distinct(model, missingness, timeproc, sign_rotation,numitems, timevar,timepoints,
+           sim) %>% 
+  group_by(model, missingness, timeproc) %>%
+  summarize(list_var=list(Hmisc::smean.cl.boot(sign_rotation))) %>% 
+  ungroup %>% 
+  mutate(mean_est=sapply(list_var, function(x) x['Mean']),
+         low_ci=sapply(list_var, function(x) x['Lower']),
+         high_ci=sapply(list_var, function(x) x['Upper'])) %>% 
+  select(-list_var)
 
-sim_draws %>% 
-  mutate(missingness=factor(missingness)) %>% 
-  ggplot(aes(y=sign_rotation,
-             x=reorder(model,sign_rotation))) +
-  stat_summary(fun.data="mean_cl_normal",
-               aes(linetype=missingness,
-                   colour=missingness),
-               position=position_dodge(.5)) +
-  facet_wrap(~timeproc,scales="free_y")
-
-sim_draws %>% 
-  mutate(timepoints=factor(timepoints)) %>% 
-  ggplot(aes(y=sign_rotation,
-             x=reorder(model,sign_rotation))) +
-  stat_summary(fun.data="mean_cl_normal",
-               aes(linetype=timepoints,
-                   colour=timepoints),
-               position=position_dodge(.5)) +
-  facet_wrap(~timeproc,scales="free_y")
-
-# now try for number of items
-
-sim_draws %>% 
-  ggplot(aes(y=rmse,
-             x=reorder(numitems,rmse))) +
-  stat_summary(fun.data="mean_cl_normal",
-               aes(colour=model,linetype=model),
-               position=position_dodge(.5)) +
-  facet_wrap(~timeproc,scales="free_y")
-
-# coverage
-
-sim_draws %>% 
-  mutate(missingness=factor(missingness)) %>% 
-  ggplot(aes(y=in_interval,
-             x=reorder(model,in_interval))) +
-  stat_summary(fun.data="mean_cl_normal",
-               aes(colour=missingness,linetype=missingness),
-               position=position_dodge(.5)) +
+p5 <-  out_sign_rotate %>% 
+  ggplot(aes(y=mean_est,
+             x=reorder(model,mean_est))) +
+  geom_pointrange(aes(ymin=low_ci, ymax=high_ci,
+                      linetype=missingness,
+                      colour=missingness),
+                  position=position_dodge(.5)) +
   facet_wrap(~timeproc,scales="free_y") +
-  geom_hline(yintercept = .95, linetype=2)
+  #geom_hline(yintercept = 0.95, linetype=2) +
+  labs(y="RMSE",x="",
+       caption=stringr::str_wrap("Plot shows averages with 5% to 95% CIs for RMSE of true to estimated ideal point scores. Facets show different true time series processes used to generate the data and for idealstan/Pathfinder/Laplace methods, also used to estimate.",
+                                 width=60)) + 
+  scale_x_discrete(guide = guide_axis(n.dodge = 2)) +
+  ggthemes::theme_clean() +
+  theme(legend.position = "top")
 
-# time
+saveRDS(p5,"data/plot_sign_rotate.rds")
 
-mean_fun <- function(x) {
-  
-  round(mean(x,na.rm=T),1)
-  
-}
+ggsave("plots/sign_rotate.png",plot=p5)
 
-sim_draws %>% 
+p6 <- sim_draws %>% 
   mutate(time_elapsed=as.numeric(time_elapsed)/60) %>% 
-  ggplot(aes(y=time_elapsed,
-             x=reorder(timeproc, time_elapsed))) +
-  stat_summary(fun.data="mean_cl_normal",
-               #aes(colour=model,linetype=model),
-               position=position_dodge(.5)) +
-  facet_wrap(~model) +
-  stat_summary(fun=mean_fun, geom="text",vjust=1, aes(label=..y..)) +
+  mutate(missingness=factor(missingness,labels=c("Ignorable","Non-ignorable")),
+         timeproc=factor(timeproc, levels=c("AR","GP","randomwalk","splines"),
+                         labels=c("AR(1)","Gaussian Process","Random Walk","Spline"))) %>% 
+  distinct(time_elapsed, timeproc, model) %>% 
+  group_by(timeproc, model) %>% 
+  summarize(mean_time=mean(time_elapsed,na.rm=T)) %>% 
+         mutate(time_label=round(mean_time, 2)) %>% 
+  ggplot(aes(y=mean_time,
+             x=reorder(model, mean_time))) +
+  geom_col() +
+  geom_text(aes(label=time_label),nudge_y=4) +
+  scale_x_discrete(guide = guide_axis(n.dodge = 2)) +
+  facet_wrap(~timeproc) +
   labs(y="Average # of Minutes per Run",
        x="") +
-  scale_y_log10()
+  ggthemes::theme_clean()
 
+saveRDS(p6,"data/plot_time_elapsed.rds")
 
-calc_sum %>% 
-  group_by(model) %>% 
-  summarize(mean_m_error=mean(m_error,na.rm=T)) %>% 
-  arrange(mean_m_error)
-
-sim_draws %>% 
-  ggplot(aes(x=ideal_point)) +
-  geom_histogram() +
-  facet_wrap(~model)
-
-sim_draws %>% 
-  mutate(timevar=factor(timevar)) %>% 
-  ggplot(aes(x=rmse)) +
-  geom_density(aes(fill=timevar),alpha=0.5) +
-  facet_wrap(~model)
-
-# coverage
-
-calc_sum %>%
-  ggplot(aes(x=cov_ideal)) +
-  geom_histogram() +
-  facet_wrap(~model)
-
-
-
-sim_draws %>% 
-  group_by(model) %>% 
-  mutate(mean_rmse=mean(rmse,na.rm=T)) %>% 
-  ggplot(aes(x=rmse)) +
-  geom_histogram() +
-  geom_vline(aes(xintercept=mean_rmse),linetype=2,colour="red") +
-  facet_wrap(~model + timevar)
-
-sim_draws %>% 
-  group_by(model,sim) %>% 
-  summarize(mean_rmse=mean(rmse, na.rm=T),
-coef_rmse=rmse(as.numeric(true_est_coef) - as.numeric(est_coef))) %>% 
-  ggplot(aes(x=coef_rmse,y=mean_rmse)) +
-  geom_point() +
-  facet_wrap(~model)
-
-# see if DW NOMINATE convergence failures are correlated with greater rmse
-
-wide_sims <- sample_n(ungroup(sim_draws), 3000) 
-
-wide_sims <- wide_sims %>% 
-  dplyr::select(model, sim, person_id, time_id, rmse,
-                missingness,timevar,timeproc,nsims) %>% 
-  spread(key = "model",value="rmse") %>% 
-  mutate(dw_miss=as.numeric(is.na(`DW-NOMINATE`)))
-
-dw_errs <- lm(dw_miss ~ idealstan + emIRT + MCMCpack + timevar, data=wide_sims) 
-
-# library(missRanger)
-# 
-# dw_impute <- missRanger(wide_sims)
-# 
-# # merge with original data
-# 
-# wide_sims <- left_join(wide_sims, 
-#                        select(dw_impute, dw_impute="DW-NOMINATE", sim:nsims),
-#                        by=c("sim",'person_id',"time_id",'missingness',
-#                             "timevar",'timeproc'))
-# 
-# group_by(wide_sims, dw_miss) %>% 
-#   summarize(mean_miss_rmse=mean(dw_impute))
+ggsave("plots/time_elapsed.png",plot=p6)
